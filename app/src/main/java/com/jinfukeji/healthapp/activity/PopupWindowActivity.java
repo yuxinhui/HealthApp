5f536e89cce353539ce84fea4fedd7ae4a256526
yuxiunhui <yuxinhui000000>
2017-04-11 17:59:01 +0800

    填充图片

--- app/src/main/java/com/jinfukeji/healthapp/activity/PopupWindowActivity.java
+++ app/src/main/java/com/jinfukeji/healthapp/activity/PopupWindowActivity.java
@@ -1,10 +1,14 @@
 package com.jinfukeji.healthapp.activity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -22,12 +26,32 @@ public class PopupWindowActivity extends AppCompatActivity{
     EditText chanpinhao_et,mima_et;
     Button quxiao_btn,baocun_btn;
     LinearLayout popup_jiqipeizhi_ll;
+    Dialog dialog;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.popup_window);
-        initView();
-        initOnclick();
+        dialog=new Dialog(this,R.style.PeizhiPopupWindow);
+        dialog.setContentView(R.layout.popup_window);
+        dialog.show();
+        View.OnClickListener listener=new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.quxiao_btn:
+                        //取消
+                        finish();
+                        break;
+                }
+                dialog.dismiss();
+            }
+        };
+        initView(listener);
+        Window window=dialog.getWindow();
+        window.getDecorView().setPadding(0,0,0,0);
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.width = LinearLayout.LayoutParams.MATCH_PARENT;
+        params.gravity = Gravity.BOTTOM;
+        window.setAttributes(params);
     }
 
     @Override
@@ -36,23 +60,14 @@ public class PopupWindowActivity extends AppCompatActivity{
         return true;
     }
 
-    //点击事件
-    private void initOnclick() {
-        quxiao_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //销毁弹窗
-                finish();
-            }
-        });
-    }
-
     //控件初始化
-    private void initView() {
-        popup_jiqipeizhi_ll= (LinearLayout) this.findViewById(R.id.popup_jiqipeizhi_ll);
-        chanpinhao_et= (EditText)findViewById(R.id.chanpinhao_et);
-        mima_et= (EditText) findViewById(R.id.mima_et);
-        quxiao_btn= (Button) findViewById(R.id.quxiao_btn);
-        baocun_btn= (Button)findViewById(R.id.baocun_btn);
+    private void initView(View.OnClickListener listener) {
+        popup_jiqipeizhi_ll= (LinearLayout) dialog.findViewById(R.id.popup_jiqipeizhi_ll);
+        chanpinhao_et= (EditText)dialog.findViewById(R.id.chanpinhao_et);
+        mima_et= (EditText) dialog.findViewById(R.id.mima_et);
+        quxiao_btn= (Button) dialog.findViewById(R.id.quxiao_btn);
+        baocun_btn= (Button)dialog.findViewById(R.id.baocun_btn);
+
+        quxiao_btn.setOnClickListener(listener);
     }
 }
